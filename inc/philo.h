a463562481bad0af1286f228cc6825f30460a31a
kicuma7 <josephkicuma7@gmail.com>
2024-12-04 07:31:25 +0100

    My First commit

---
+++ inc/philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jquicuma <jquicuma@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/04 07:29:21 by jquicuma          #+#    #+#             */
+/*   Updated: 2024/12/04 07:30:32 by jquicuma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <pthread.h>
+# include <stdbool.h>
+
+#endif
